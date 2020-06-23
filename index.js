d644ff9c739ed05e74ab46fb84ac78627ee15b2f
Arslan Sajid <arslansajid951@gmail.com>
2020-06-23 11:52:57 +0500

    base 64 fix

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
+import {decode, encode} from 'base-64'
+
+if (!global.btoa) {
+    global.btoa = encode;
+}
+
+if (!global.atob) {
+    global.atob = decode;
+}
 
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
