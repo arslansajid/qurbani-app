ab654d0ac1c2847cf7674397523b7470b9ea01d6
Arslan Sajid <arslansajid951@gmail.com>
2020-06-25 11:51:34 +0500

    City selection check

--- src/Components/CategoryCard.js
+++ src/Components/CategoryCard.js
@@ -1,12 +1,12 @@
 
 import * as React from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import { totalSize, width, height } from "react-native-dimension"
 import colors from '../Themes/Colors';
-import {Icon} from 'react-native-elements';
-import {setCategory} from '../redux/actions/CandidateFiltersActions';
-import {connect} from 'react-redux';
+import { Icon } from 'react-native-elements';
+import { setCategory } from '../redux/actions/CandidateFiltersActions';
+import { connect } from 'react-redux';
 import commonStyles from '../Containers/Styles/commonStyles';
 import Styles from '../Containers/GalleryScreens/styles/GalleryScreenStyles';
 
@@ -15,34 +15,39 @@ const CategoryCard = (props) => {
     const navigate = navigation.navigate;
 
     const onCardSelect = (value) => {
-        console.log("########### seleted value", value)
-        props.dispatch(setCategory(value));
-        navigate('SearchResultsScreen');
+        const { selectedCity } = props;
+        console.log("select city ###########", selectedCity)
+        if (!!selectedCity && !!selectedCity.name) {
+            props.dispatch(setCategory(value));
+            navigate('SearchResultsScreen')
+        } else {
+            alert('Please Select City')
+        }
     }
     return (
         <>
-        {
-            selected && (
-                <View style={{padding: totalSize(0.75), display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'absolute', left: 0, zIndex: 10, backgroundColor: colors.appColor1}}>
-                    <Icon
-                        name={'check'}
-                        type="font-awesome"
-                        color={colors.white}
-                        size={totalSize(2)}
-                        iconStyle={{zIndex: 10}}
-                    />
-                </View>
-            )
-        }
-        <TouchableOpacity
-            onPress={() => parent === "upload" ? onSelect(title) : onCardSelect(title) }
-            style={styles.cardContainer}>
-            <Card.Cover resizeMode={"cover"} style={{ height: parent === "upload" ? totalSize(12) : totalSize(15), zIndex: 1 }} source={image} />
-            <Card.Content style={{paddingBottom: 0, paddingHorizontal: 0}}>
-                {/* <Title style={{textAlign: 'center'}}>{title}</Title> */}
-                <Text style={[{textAlign: 'center', marginVertical: 3}, commonStyles.h4, commonStyles.bold]}>{title}</Text>
-            </Card.Content>
-        </TouchableOpacity>
+            {
+                selected && (
+                    <View style={{ padding: totalSize(0.75), display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'absolute', left: 0, zIndex: 10, backgroundColor: colors.appColor1 }}>
+                        <Icon
+                            name={'check'}
+                            type="font-awesome"
+                            color={colors.white}
+                            size={totalSize(2)}
+                            iconStyle={{ zIndex: 10 }}
+                        />
+                    </View>
+                )
+            }
+            <TouchableOpacity
+                onPress={() => parent === "upload" ? onSelect(title) : onCardSelect(title)}
+                style={styles.cardContainer}>
+                <Card.Cover resizeMode={"cover"} style={{ height: parent === "upload" ? totalSize(12) : totalSize(15), zIndex: 1 }} source={image} />
+                <Card.Content style={{ paddingBottom: 0, paddingHorizontal: 0 }}>
+                    {/* <Title style={{textAlign: 'center'}}>{title}</Title> */}
+                    <Text style={[{ textAlign: 'center', marginVertical: 3 }, commonStyles.h4, commonStyles.bold]}>{title}</Text>
+                </Card.Content>
+            </TouchableOpacity>
         </>
     )
 };
