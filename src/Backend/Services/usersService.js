8bfef78d3d1faff6138388b2f2dc2e4cb64dd468
Arslan Sajid <arslansajid951@gmail.com>
2020-07-13 10:53:35 +0500

    users service updated

--- src/Backend/Services/usersService.js
+++ src/Backend/Services/usersService.js
@@ -1,6 +1,6 @@
 
 import { db } from '../firebase';
-import { User } from '../models/user';
+import { User } from '../Models/user';
 
 export const getUsers = async function () {
     const query = await db.collection('Users').limit(20).get();
@@ -28,6 +28,23 @@ export const deleteUser = async function (id) {
 
 export const updateUser = async function (id, data) {
     await db.collection('Users').doc(id).set(data, { merge: true });
+};
+
+export const getUserByEmail = async function (email) {
+    console.log("Email", email)
+
+    const query = await db.collection('Users').where('email', '==', email).get();
+    let users = [];
+
+    query.docs.forEach((doc) => {
+        const user = User.fromFirestore(doc);
+        if (user) {
+            users.push(user);
+        }
+    });
+    console.log('Users', users);
+
+    return users;
 };
 
 export const getUserById = async function (id) {
