8817b8a64ac96243872536a573d97d8d51d5022b
Arslan Sajid <arslansajid951@gmail.com>
2020-07-13 10:49:00 +0500

    Updated cities

--- src/Api/static/data.js
+++ src/Api/static/data.js
@@ -54,7 +54,7 @@ export const Cities = [
         id: 6
     },
     {
-        name: ' Quetta',
+        name: 'Quetta',
         id: 7
     },
     {
@@ -62,68 +62,383 @@ export const Cities = [
         id: 8
     },
     {
-        name: 'Sialkot',
+        name: 'Abbotabad',
         id: 9
     },
     {
-        name: 'Gujranwala',
+        name: 'Ahmedpur East',
         id: 10
     },
     {
-        name: 'Jhelum',
+        name: 'Arif Wala',
         id: 11
     },
     {
-        name: 'Jhang',
+        name: 'Attock',
         id: 12
     },
-    {
-        name: 'Chakwal',
+{
+        name: 'Badin',
         id: 13
     },
     {
-        name: 'Mianwali',
+        name: 'Bahawalnagar',
         id: 14
     },
     {
-        name: 'Kasur',
+        name: 'Bahawalpur',
         id: 15
     },
     {
-        name: 'Sahiwal',
+        name: 'Bhakkar',
         id: 16
     },
     {
-        name: 'Mardan',
+        name: 'Bhalwal',
         id: 17
     },
     {
-        name: 'Abbottabad',
+        name: 'Burewala',
         id: 18
     },
     {
-        name: 'Chaman',
+        name: 'Chakwal',
         id: 19
     },
     {
-        name: 'Chitral',
+        name: 'Chaman',
         id: 20
     },
     {
-        name: 'Sawat',
+        name: 'Charsadda',
         id: 21
     },
     {
-        name: 'Muzaffarabad',
+        name: 'Chiniot',
         id: 22
     },
     {
-        name: 'Bahawalpur',
+        name: 'Chishtian',
         id: 23
     },
     {
-        name: 'DG Khan',
+        name: 'Dadu',
         id: 24
+    },
+{
+        name: 'Daharki',
+        id: 25
+    },
+    {
+        name: 'Daska',
+        id: 26
+    },
+    {
+        name: 'Dera Ghazi Khan',
+        id: 27
+    },
+    {
+        name: 'Dera Ismail Khan',
+        id: 28
+    },
+    {
+        name: 'Ferozwala',
+        id: 29
+    },
+    {
+        name: 'Ghotki',
+        id: 30
+    },
+    {
+        name: 'Gojra',
+        id: 31
+    },
+    {
+        name: 'Gujranwala',
+        id: 32
+    },
+    {
+        name: 'Gujranwala Cantonment',
+        id: 33
+    },
+    {
+        name: 'Gujrat',
+        id: 34
+    },
+    {
+        name: 'Hafizabad',
+        id: 35
+    },
+    {
+        name: 'Haroonabad',
+        id: 36
+    },
+    {
+        name: 'Hasilpur',
+        id: 37
+    },
+    {
+        name: 'Hub',
+        id: 38
+    },
+    {
+        name: 'Hyderabad',
+        id: 39
+    },
+    {
+        name: 'Jacobabad',
+        id: 40
+    },
+    {
+        name: 'Jaranwala',
+        id: 41
+    },
+    {
+        name: 'Jatoi',
+        id: 42
+    },{
+        name: 'Jhang',
+        id: 43
+    },
+    {
+        name: 'Jhelum',
+        id: 44
+    },
+    {
+        name: 'Kabal',
+        id: 45
+    },
+    {
+        name: 'Kamalia',
+        id: 46
+    },
+    {
+        name: 'Kamber Ali Khan',
+        id: 47
+    },
+    {
+        name: 'Kāmoke',
+        id: 48
+    },
+{
+        name: 'Kandhkot',
+        id: 49
+    },
+    {
+        name: 'Kasur',
+        id: 50
+    },
+    {
+        name: 'Khairpur',
+        id: 51
+    },
+    {
+        name: 'Khanewal',
+        id: 52
+    },
+    {
+        name: 'Khanpur',
+        id: 53
+    },
+    {
+        name: 'Khushab',
+        id: 54
+    },
+    {
+        name: 'Khuzdar',
+        id: 55
+    },
+    {
+        name: 'Kohat',
+        id: 56
+    },
+    {
+        name: 'Kot Abdul Malik',
+        id: 57
+    },
+    {
+        name: 'Kot Addu',
+        id: 58
+    },
+    {
+        name: 'Kotri',
+        id: 59
+    },
+    {
+        name: 'Larkana',
+        id: 60
+    },
+{
+        name: 'Layyah',
+        id: 61
+    },
+    {
+        name: 'Lodhran',
+        id: 62
+    },
+    {
+        name: 'Mandi Bahauddin',
+        id: 63
+    },
+    {
+        name: 'Mansehra',
+        id: 64
+    },
+    {
+        name: 'Mardan',
+        id: 65
+    },
+    {
+        name: 'Mianwali',
+        id: 66
+    },
+    {
+        name: 'Mingora',
+        id: 67
+    },
+    {
+        name: 'Mirpur',
+        id: 68
+    },
+    {
+        name: 'Mirpur Khas',
+        id: 69
+    },
+    {
+        name: 'Mirpur Mathelo',
+        id: 70
+    },
+    {
+        name: 'Muridke',
+        id: 71
+    },
+    {
+        name: 'Muzaffarabad',
+        id: 72
+    },
+{
+        name: 'Muzaffargarh',
+        id: 73
+    },
+    {
+        name: 'Narowal',
+        id: 74
+    },
+    {
+        name: 'Nawabshah',
+        id: 75
+    },
+    {
+        name: 'Nowshera',
+        id: 76
+    },
+    {
+        name: 'Okara',
+        id: 77
+    },
+    {
+        name: 'Pakpattan',
+        id: 78
+    },
+    {
+        name: 'Rahim Yar Khan',
+        id: 79
+    },
+    {
+        name: 'Sadiqabad',
+        id: 80
+    },
+    {
+        name: 'Sahiwal',
+        id: 81
+    },
+    {
+        name: 'Sambrial',
+        id: 82
+    },
+    {
+        name: 'Samundri',
+        id: 83
+    },
+    {
+        name: 'Sargodha',
+        id: 84
+    },
+    {
+        name: 'Shahdadkot',
+        id: 85
+    },
+    {
+        name: 'Sheikhupura',
+        id: 86
+    },
+    {
+        name: 'Shikarpur',
+        id: 87
+    },
+    {
+        name: 'Sialkot',
+        id: 88
+    },
+    {
+        name: 'Sukkur',
+        id: 89
+    },
+    {
+        name: 'Swabi',
+        id: 90
+    },
+    {
+        name: 'Tando Adam',
+        id: 91
+    },
+    {
+        name: 'Tando Allahyar',
+        id: 92
+    },
+    {
+        name: 'Tando Muhammad Khan',
+        id: 93
+    },
+    {
+        name: 'Taxila',
+        id: 94
+    },
+    {
+        name: 'Turbat',
+        id: 95
+    },
+    {
+        name: 'Umerkot',
+        id: 96
+    },
+    {
+        name: 'Vehari',
+        id: 97
+    },
+    {
+        name: 'Wah Cantonment',
+        id: 98
+    },
+    {
+        name: 'Wazirabad',
+        id: 99
+    },
+    {
+        name: 'Zhob',
+        id: 100
+    },
+    {
+        name: 'Gilgit',
+        id: 101
+    },
+    {
+        name: 'Skardu',
+        id: 102
+    },
+    {
+        name: 'Pasu',
+        id: 103
     },
 ]
 
